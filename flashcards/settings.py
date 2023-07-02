715248f4cdc82777b96b564743059758811772e0
Kamil <bil@int.pl>
2023-07-02 18:06:51 +0200

    Added third record.

--- flashcards/settings.py
+++ flashcards/settings.py
@@ -131,7 +131,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS = ['https://flashcardsdemoapp.azurewebsites.net/', 'https://www.flashcardsdemoapp.azurewebsites.net/']
+CSRF_TRUSTED_ORIGINS = ['https://flashcardsdemoapp.azurewebsites.net/',
+                        'https://www.flashcardsdemoapp.azurewebsites.net/', 'flashcardsdemoapp.azurewebsites.net']
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
